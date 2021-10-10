4e5abc78fa804913b282970646f6437b4400c8d0
Petrov Pavel <pouligorson@mail.ru>
2021-10-10 23:06:34 +0300

    Create class Ball, class Platform.

---
+++ break_out.py
@@ -0,0 +1,95 @@
+import pygame
+import sys
+from random import randint as rand
+from math import pi
+from simple_ball import Ball
+
+fps = 30
+
+def rand_angle():
+  angle = pi/2 + rand(-30, 30)*pi/180
+  return angle
+
+class Ball(Ball):
+  def collide(self, pf):
+    x = self.geometry.x
+    y = self.geometry.y
+    if y + self.radius >= pf.y - pf.h:
+      if pf.x <= x <= pf.x + pf.w:
+        self.move_y = -self.move_y
+
+class Platform():
+  def __init__(self, screen):
+    self.size = pygame.display.get_surface().get_size()
+    self.w, self.h = 80, 10
+    self.x = self.size[0]/2 - self.w/2
+    self.y = self.size[1]*0.9
+    self.collor = (50, 255, 50)
+    self.turns = 0
+    self.speed = 3
+  
+  def moves(self):
+    self.x += self.speed * self.turns
+  
+  def turn(self, t):
+    if t == 1:
+      self.turns = 1
+    elif t == -1: self.turns = -1
+    else: self.turns = 0
+
+  def draw(self, screen):
+    pygame.draw.rect(screen, self.collor, (self.x, self.y, self.w, self.h))
+
+def break_out():
+  size = width, height = 350, 350
+  pygame.init()
+  pygame.display.set_caption('BreakOut')
+  screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+  BLACK = 0, 0, 0
+
+  ball = Ball(screen)
+  pf = Platform(screen)
+
+  game_over = False
+  while not game_over:
+    # события
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        game_over = True
+      
+      if event.type == pygame.VIDEORESIZE:
+        size = width, height = event.w, event.h
+        screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+      
+      if event.type == pygame.KEYDOWN:
+        if pygame.K_a == event.key:
+          pf.turn(-1)
+        
+        if pygame.K_d == event.key:
+          pf.turn(1)
+      
+      if event.type == pygame.KEYUP:
+        if pygame.K_a == event.key or pygame.K_d == event.key:
+          pf.turn(0)
+    
+    # логика работы игры
+    ball.collide(pf)
+    ball.move()
+    pf.moves()
+
+    # отрисовка
+    screen.fill(BLACK)
+    ball.draw(screen)
+    pf.draw(screen)
+
+    pygame.display.flip()
+
+    # подождать
+    pygame.time.wait(fps)
+    
+  sys.exit()
+
+
+if __name__ == '__main__':
+  break_out()
---
+++ simple_ball.py
@@ -0,0 +1,80 @@
+import pygame
+import sys
+from random import randint as rand
+from math import pi, cos, sin
+
+fps = 30
+
+def rand_angle():
+  sign = -1 if rand(0, 1) == 0 else 1
+  angle = pi/2 + sign * rand(30, 90)*pi/180
+  return angle
+
+class Ball():
+  def __init__(self, screen, angle=rand_angle()):
+    self.size = pygame.display.get_surface().get_size()
+    self.image = pygame.image.load('basketball.png')
+    self.radius = 13
+    self.image = pygame.transform.scale(self.image, (self.radius*2, self.radius*2))
+    self.geometry = self.image.get_rect()
+    self.geometry.centerx = self.size[0]/2
+    self.geometry.centery = self.size[1]*0.8
+    self.move_x = cos(angle)
+    self.move_y = -sin(angle)
+    self.speed = 4
+
+  def draw(self, screen):
+    screen.blit(self.image, self.geometry)
+  
+  def move(self):
+    if 0 >= self.geometry.centerx - self.radius:
+      self.move_x = -self.move_x
+      self.geometry.x += 2
+    elif self.size[0] <= self.geometry.centerx + self.radius:
+      self.move_x = -self.move_x
+      self.geometry.x -= 2
+
+    if 0 >= self.geometry.y:
+      self.move_y = -self.move_y
+      self.geometry.y += 2
+    
+    self.geometry.centerx += self.move_x * self.speed
+    self.geometry.centery += self.move_y * self.speed
+
+def simple_ball():
+  size = width, height = 350, 350
+  pygame.init()
+  pygame.display.set_caption('BreakOut')
+  screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+  BLACK = 0, 0, 0
+
+  ball = Ball(screen)
+
+  game_over = False
+  while not game_over:
+    # события
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        game_over = True
+      
+      if event.type == pygame.VIDEORESIZE:
+        size = width, height = event.w, event.h
+        screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    
+    # логика работы игры
+    ball.move()
+
+    # отрисовка
+    screen.fill(BLACK)
+    ball.draw(screen)
+
+    pygame.display.flip()
+
+    # подождать
+    pygame.time.wait(fps)
+    
+  sys.exit()
+
+if __name__ == '__main__':
+    simple_ball()
